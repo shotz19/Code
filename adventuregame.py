84e1d126252251424d609d95b3ce7330664b0794
Steph <shotz19@choate.edu>
2018-09-21 11:04:04 -0400

    a

--- adventuregame.py
+++ adventuregame.py
@@ -1,4 +1,17 @@
 import random
+def town():
+	print("You go into a town and see an inn where you can stay the night.\nYou hang out there for the night and decide to continue your adventure in the morning.\n\n")
+	killthedragon()
+def sphinx():
+	print("The sphinx is sitting on a rock and says to you\nI have a riddle for you.")
+	riddle=input("Voiceless it cries\nWingless flutters\nToothless bites\nMouthless mutters.")
+	if riddle == "wind" or riddle == "Wind" or riddle=="the wind":
+		print("You have solved the sphinx's riddle!\nIt tells you a shortcut through the mountain, sp you don't have to climb a treacherous pass.")
+	elif riddle == "bug":
+		print("The sphinx hates your answer, so it kills you.")
+		exit()
+	else:
+		print("The sphinx does not like your answer!\nYou must continue on your original path. \nYou are lucky you survived an encounter with this dangerous creature! ")
 def impass():
 	print("You continue walking up the mountain and you come upon a steep impass.\nYou must climb this impass to contiue.\nTo go through, you must play a game and win.")
 	print("When I show you a letter, match it on your keyboard to climb the mounatin")
@@ -14,23 +27,23 @@ def impass():
 			exit()
 	print("Yay! You climbed the impass")
 def killthedragon():
-	print("You have chosen to kill the dragon")
 	print("You walk up to a giant, looming mountain")
 	print("You are told that this is where the dragon lives.")
-	sphinx=input("Along the way you encounter a sphinx would you like to\n\n1) talk to it\n\n2)continue on")
-	if sphinx=="1":
+	sphinxi=input("Along the way you encounter a sphinx would you like to\n\n1) talk to it\n\n2)continue on")
+	if sphinxi=="1":
 		sphinx()
-	elif sphinx=="2":
+	elif sphinxi=="2":
 		print("You have chosen to ignore the sphinx and continue on your path")
 		impass()
 	else:
-		print("I don't know what "+sphinx+"means. Please type 1 or 2")
+		print("I don't know what "+sphinxi+"means. Please type 1 or 2")
 		killthedragon()
 	impass()
 def start():
 	print("\nYou are here to kill a dragon!\n\nThe dragon has been terrorizing this city for years.")
 	one=input("\nYou find yourself at a fork in the road. \n\nWould you like to \n\n1) continue on \n\n2) go to a town where you can hang out")
 	if one=="1": 
+		print("You have chosen to kill the dragon")
 		killthedragon()
 	elif one=="2":
 		town()
@@ -38,8 +51,6 @@ def start():
 		print("I don't know what "+one+"means. Please type 1 or 2")
 		start()
 start()
-def town():
-	print("You go into a town...")
 def sphinx():
 	print("The sphinx is sitting on a rock and says to you\nI have a riddle for you.")
 	riddle=input("Voiceless it cries\nWingless flutters\nToothless bites\nMouthless mutters.")
