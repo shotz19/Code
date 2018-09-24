90fa18017d76bde2bc361470458899de74f1f47c
Steph <shotz19@choate.edu>
2018-09-24 10:47:18 -0400

    k

--- adventuregame.py
+++ adventuregame.py
@@ -12,6 +12,26 @@ def sphinx():
 		exit()
 	else:
 		print("The sphinx does not like your answer!\nYou must continue on your original path. \nYou are lucky you survived an encounter with this dangerous creature! ")
+def dragon():
+	print("You have found the dragon!")
+def game():
+	riddle=input("The goblin looks up at you and gives you a piece of paper that says:\n\nThis thing all things devours\nBirds, beasts, trees, flowers;\nGnaws iron, bites steel\nGrinds hard stones to meal\nSlays king, ruins town\nAnd beats high mountain down.")
+	if riddle == "time"
+		print("The goblin shows you a secret passageway through the cave that leads you to the dragon's layer.")
+		dragon()
+	else:
+		print("The goblin is angry with yiur stupid answer, so he chases you out of his cave.\nYou walk for several more miles until you come upon what seems to be the dragon's layer.")
+		dragon()
+def cave():
+	cavei=input("You come upon a giant cave. Do you\n\n1)Go in\n\n2)Ignore it and contniue walking.\n\nChoose 1 or 2")
+	if cavei=="1":
+		print("You have chosen to go into the cave!\nYou walk into the cave and see a small goblin creature that asks you to play a game with it.\n It brings you deeper into the cave and gives you cards.")
+		game()
+	elif cavei =="2":
+		("You continue on your path and will never get to learn what lies within the cave.")
+	else:
+		print("I do not understand. Please enter 1 or 2")
+		cave()
 def impass():
 	print("You continue walking up the mountain and you come upon a steep impass.\nYou must climb this impass to contiue.\nTo go through, you must play a game and win.")
 	print("When I show you a letter, match it on your keyboard to climb the mounatin")
@@ -26,6 +46,7 @@ def impass():
 			print("You missed ther letter! You failed to grab the next handhold and you fall to your death :(")
 			exit()
 	print("Yay! You climbed the impass")
+	cave()
 def killthedragon():
 	print("You walk up to a giant, looming mountain")
 	print("You are told that this is where the dragon lives.")
@@ -56,9 +77,11 @@ def sphinx():
 	riddle=input("Voiceless it cries\nWingless flutters\nToothless bites\nMouthless mutters.")
 	if riddle == "wind" or riddle == "Wind" or riddle=="the wind":
 		print("You have solved the sphinx's riddle!\nIt tells you a shortcut through the mountain, sp you don't have to climb a treacherous pass.")
+		cave()
 	elif riddle == "bug":
 		print("The sphinx hates your answer, so it kills you.")
 		exit()
 	else:
 		print("The sphinx does not like your answer!\nYou must continue on your original path. \nYou are lucky you survived an encounter with this dangerous creature! ")
+		impass()
 
