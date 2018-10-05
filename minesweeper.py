1ec26fa30a819d40926ff0605574b88418429e42
Steph <shotz19@choate.edu>
2018-10-05 11:05:12 -0400

    updates

--- minesweeper.py
+++ minesweeper.py
@@ -5,10 +5,12 @@ import random
 W=int(sys.argv[1])
 H=int(sys.argv[2])
 B=int(sys.argv[3])
+score=0
 if B>W*H:
 	print("NO!")
 	exit()
 board=[[0]*(W+2) for i in range(H+2)]
+board1=[["o"]*(W+2) for i in range(H+2)]
 bombs=0
 while bombs<B:
 	bomby=random.randint(0,H-1)
@@ -41,8 +43,28 @@ for x in range(H):
 for x in range(H):
 	del board[x][W]
 	del board[x][W]
-for x in range(len(board)-2):
-	print(*board[x])
+for x in range(H):
+	del board1[x][W]
+	del board1[x][W]
+'''for x in range(len(board)-2):
+	print(*board[x])'''
+for x in range(len(board1)-2):
+	print(*board1[x])
+while True:
+	row=int(input("What space would you like to open?\nPlease enter the row"))-1
+	column=int(input("Now, enter the comlumn"))-1
+	board1[row][column]=board[row][column]
+	for x in range(len(board1)-2):
+		print(*board1[x])
+	if board[row][column]=='*':
+		print("You lose")
+		exit()
+	else:
+		score=score+1
+	if score==(W*H)-B:
+		print("You win!")
+		exit()
+
 
 	
 
