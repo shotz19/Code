af7cf15df7ea71102013e3cecb831e5f6516af85
Steph <shotz19@choate.edu>
2018-10-05 16:37:06 -0400

    finishes radiation

--- minesweeper.py
+++ minesweeper.py
@@ -6,6 +6,39 @@ W=int(sys.argv[1])
 H=int(sys.argv[2])
 B=int(sys.argv[3])
 score=0
+def printboard():
+	for x in range(H):
+		del board[x][W]
+		del board[x][W]
+	for x in range(H):
+		del board1[x][W]
+		del board1[x][W]
+	for x in range(len(board1)-2):
+		print(*board1[x])
+	for x in range (H):
+		board[x].append(0)
+		board[x].append(0)
+	for x in range (H):
+		board1[x].append(0)
+		board1[x].append(0)
+def printaround(therow,thecolumn,score):
+	score=score
+	board1[therow+1][thecolumn]= board[therow+1][thecolumn]
+	score=score+1
+	board1[therow-1][thecolumn]= board[therow-1][thecolumn]
+	score=score+1
+	board1[therow][thecolumn+1]= board[therow][thecolumn+1]
+	score=score+1
+	board1[therow][thecolumn-1]= board[therow][thecolumn-1]
+	score=score+1
+	board1[therow+1][thecolumn+1]= board[therow+1][thecolumn+1]
+	score=score+1
+	board1[therow+1][thecolumn-1]= board[therow+1][thecolumn-1]
+	score=score+1
+	board1[therow-1][thecolumn+1]= board[therow-1][thecolumn+1]
+	score=score+1
+	board1[therow-1][thecolumn-1]= board[therow-1][thecolumn-1]
+	score=score+1
 if B>W*H:
 	print("NO!")
 	exit()
@@ -40,30 +73,40 @@ for x in range(H):
 			if board[x-1][y-1]=="*":
 				count=count+1
 			board[x][y]=count
-for x in range(H):
-	del board[x][W]
-	del board[x][W]
-for x in range(H):
-	del board1[x][W]
-	del board1[x][W]
-'''for x in range(len(board)-2):
-	print(*board[x])'''
-for x in range(len(board1)-2):
-	print(*board1[x])
+printboard()
+score=0
 while True:
-	row=int(input("What space would you like to open?\nPlease enter the row"))-1
-	column=int(input("Now, enter the comlumn"))-1
-	board1[row][column]=board[row][column]
-	for x in range(len(board1)-2):
-		print(*board1[x])
-	if board[row][column]=='*':
-		print("You lose")
-		exit()
+	flag=input("Would you like to\n\n1) Guess a space\n\nor\n\n2)Place a flag")
+	if flag=="1":
+		row=int(input("What space would you like to open?\nPlease enter the row"))-1
+		column=int(input("Now, enter the comlumn"))-1
+		board1[row][column]=board[row][column]
+		if board[row][column]=='*':
+			printboard()
+			print("You lose")
+			exit()
+		elif board[row][column]==0:
+			score=score+1
+			printaround(row,column,score)
+			for x in range(H):
+				for y in range(W):
+					if board[x][y]==0 and board1[x][y]==board[x][y]:
+						printaround(x,y,score)
+			printboard()	
+		else:
+			score=score+1
+			printboard()
+		if score==(W*H)-B:
+			print("You win!")
+			exit()
+	elif flag=="2":
+		row=int(input("Where would you like to place a flag?\nPlease enter the row"))-1
+		column=int(input("Now, enter the comlumn"))-1
+		board1[row][column]="*"
+		for x in range(len(board1)-2):
+			print(*board1[x])
 	else:
-		score=score+1
-	if score==(W*H)-B:
-		print("You win!")
-		exit()
+		print("I do not understand.")
 
 
 	
---
+++ testingstuff.py
@@ -0,0 +1,6 @@
+hi=input("say hi")
+boo=input("say boo")
+if hi=="hi" and boo=="boo":
+	print("yes")
+else:
+	print("no")
