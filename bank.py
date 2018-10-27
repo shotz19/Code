d1fe6e21e9a0a1cfd609b048bbb05f1e741cb8dc
Steph <shotz19@choate.edu>
2018-10-27 09:37:11 -0400

    bank

--- bank.py
+++ bank.py
@@ -1,33 +1,43 @@
 class Account:
-	def __init__(self,name,balance,pin):
+	def __init__(self,name,balance,pin,amount):
 		self.name=name
 		self.balance=balance
 		self.pin=pin
-	def withdraw(self, amount):
+		self.amount=amount
+	def withdraw(self):
 		if self.amount<self.balance:
 			self.balance=self.balance-amount
 			status="You now have"+str(self.balance)
 		else:
 			status="You can not do that!"	
 		return status
-	def deposit(self, amount):
+	def deposit(self):
 		self.balance=self.balance+amount
 		status="You now have"+str(self.balance)
 	def stats(self):
 		info="Name: "+self.name
 		info+="\nBalance "+str(self.balance)
-		info+="\nPin: "+str(self.pin)
 		return info
-account1=Account("Steph",300, 9999)
+account1=Account("Steph",300, 9999,0)
 while True:
 	print(account1.stats())
-	choice = input("What would you like to do")
+	choice = input("What would you like to do?\n")
 	if choice== "withdraw":
-		amount=int(input("How much?"))
-		print("\n"+account1.withdraw(amount))
+		pinnum=int(input("\nWhat is you pin number?\n"))
+		if pinnum== int(account1.pin):
+			amount=int(input("\nHow much?"))
+			account1.amount=amount
+			print("\n"+account1.withdraw())
+		else:
+			print("\nThat is not your pin number. You may not make a transaction.")
 	elif choice=="deposit":
-		amount=int(input("How much?"))
-		print("\n"+account1.deposit(amount))
+		pinnum=int(input("\nWhat is you pin number?\n"))
+		if pinnum== int(account1.pin):
+			amount=int(input("How much?"))
+			account1.amount=amount
+			print("\n"+account1.deposit())
+		else:
+			print("\nThat is not your pin number. You may not make a transaction.")
 	elif choice=="end":
 		exit()
 	else:
