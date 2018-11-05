d047ddeffe22af878e4a2e2b258d3516c5758ff1
Steph <shotz19@choate.edu>
2018-11-05 10:02:30 -0500

    elements with statrt of element class and finished with list of elements

--- elements.py
+++ elements.py
@@ -1,9 +1,24 @@
 #https://stackoverflow.com/questions/18412179/reading-a-file-and-storing-values-into-variables-in-python
 file = open('elements.csv')
-for line in file:
-	element=line.readline()
+'''for line in file:
+	element= file.readline()
 	part = line.split(',')
-	print (part[0],part[1])
-'''first = f.readline()
-second = f.readline()
-print(first,second)'''
+	print (part[0],part[1])'''
+board=[]
+for i in range (0,103):
+	for line in file:
+		element = file.readline()
+		part = line.split(',')
+		elements=[part[0],part[1],part[2],part[3],part[4]]
+		board.append(elements)
+print(board[0][0])
+class elements:
+	def __init__ (self,Element,Number,Symbol,Weight,Boil):
+		self.Element = Element
+		self.Number = Number
+		self.Symbol = Symbol
+		self.Weight = Weight
+		self.Boil = Boil
+	def add(self):
+		addition= self.Weight + self.weight
+
