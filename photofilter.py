104be8ac543ff787d345f87c37f058e8112f9b42
Steph <shotz19@choate.edu>
2018-11-16 11:48:17 -0500

    photofilter

---
+++ photofilter.py
@@ -0,0 +1,25 @@
+#https://docs.python-guide.org/scenarios/imaging/
+import sys
+def filterpic(pic):
+	from PIL import Image, ImageFilter
+	originalimage = Image.open(pic)
+	imgx, imgy  = originalimage.size
+	#originalimage.show()
+	image=Image.new("RGB",(imgx, imgy))
+	count = 0
+	for x in range (imgx):
+		for y in range (imgy):
+			RGB = originalimage.getpixel((x,y))
+			r,g,b = RGB 
+			count = 0
+			if 10<x<imgx-10 and 10<y<imgy-10:
+				r = int(r + 1.5*x)
+				g = int(g + 1.5*abs(imgx/2-x))
+				b = int(b + 1.5*abs(imgx-x))
+			else:
+				r = 0
+				g = 0
+				b = 0
+			image.putpixel((x,y),(r,g,b))
+	image.show()
+filterpic(sys.argv[1])
